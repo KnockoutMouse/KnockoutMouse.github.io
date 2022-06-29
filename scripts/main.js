d35a19e64ca0eef5551de8c4ef01efaad17c5034
KnockoutMouse <52261649+KnockoutMouse@users.noreply.github.com>
2022-06-29 14:33:51 -0700

    asdf

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function() {
         myImage.setAttribute('src', 'GithubLogo.png');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'I know your name, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'I know your name, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
